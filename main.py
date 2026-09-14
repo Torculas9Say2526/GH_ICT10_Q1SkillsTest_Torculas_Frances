eb3464b3cfea8bf959c5e309527fc0b8b1583bf4
Torculas10Say2627 <torculas.fd@obmontessori.edu.ph>
2026-09-14 11:24:38 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from pyscript import document, display 
+
+
+def create_order(e):
+    matcha_ichigo = document.getElementById("item1")
+    dark_matcha = document.getElementById("item2")
+    cold_strawberry_yuzu = document.getElementById("item3")
+    mint_choco_mochi = document.getElementById("item4")
+    white_caramel_frappucino = document.getElementById("item5")
+
+    subtotal = float(matcha_ichigo.value) * float(matcha_ichigo.checked) + float(dark_matcha.value) * float(dark_matcha.checked) + float(cold_strawberry_yuzu.value) * float(cold_strawberry_yuzu.checked) + float(mint_choco_mochi.value) * float(mint_choco_mochi.checked) + float(white_caramel_frappucino.value) * float(white_caramel_frappucino.checked)
+    VAT = subtotal * 0.12
+    total = subtotal + VAT
+    display(f"Here's your receipt: ₱{total: .2f}", target="show")
