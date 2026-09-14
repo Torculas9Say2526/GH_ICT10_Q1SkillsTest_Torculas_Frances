bcf7cae00a58b1bcb4cdc03d34abf7a5862ba730
Torculas10Say2627 <torculas.fd@obmontessori.edu.ph>
2026-09-14 11:48:12 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -11,4 +11,7 @@ def create_order(e):
     subtotal = float(matcha_ichigo.value) * float(matcha_ichigo.checked) + float(dark_matcha.value) * float(dark_matcha.checked) + float(cold_strawberry_yuzu.value) * float(cold_strawberry_yuzu.checked) + float(mint_choco_mochi.value) * float(mint_choco_mochi.checked) + float(white_caramel_frappucino.value) * float(white_caramel_frappucino.checked)
     VAT = subtotal * 0.12
     total = subtotal + VAT
-    display(f"Here's your receipt: ₱{total: .2f}", target="show")
+
+    display(f"Your subtotal is: ₱{subtotal: .2f}", target="show")
+    display(f"Your VAT is: ₱{VAT: .2f}", target="show")
+    display(f"Your total is: ₱{total: .2f}", target="show")
